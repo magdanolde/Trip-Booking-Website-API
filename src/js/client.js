8e8304a910cf265a72efc95c42019cf19171f71d
Magdalena Nolde <magdalena@nolde.de>
2022-03-02 13:56:06 +0100

    customer in a making

--- src/js/client.js
+++ src/js/client.js
@@ -18,8 +18,8 @@ function loadExcursions() {
 }
 
 function insertExcursions(data) {
-    const ulEl = findUl();
-    const excursionPrototype = findPrototype();
+    const ulEl = document.querySelector(".excursions");
+    const excursionPrototype = document.querySelector(".excursions__item--prototype");
     data.forEach((item) => {
         console.log(item);
         let liEl = excursionPrototype.cloneNode(true);
@@ -34,17 +34,110 @@ function insertExcursions(data) {
         const id = getIdFromRoot(liEl);
         liEl.classList.remove("excursions__item--prototype");
         ulEl.appendChild(liEl);
+        addExcursions(liEl);
     })
 }
 
+// ADD EXCURSIONS TO THE BASKET
+
+function addExcursions(liEl) {
+    liEl.querySelector(".excursions__field--submit").addEventListener("click", event => {
+    event.preventDefault();
+    const adultNumber = liEl.querySelector("input[name=adults]").value;
+    const childNumber = liEl.querySelector("input[name=children]").value;
+    const name = liEl.querySelector(".excursions__title").textContent;
+    const childPrice = liEl.querySelector(".excursions__childprice").textContent;
+    const adultPrice = liEl.querySelector(".excursions__adultprice").textContent;
+    const id = getIdFromRoot(liEl);
+    addItem(name, adultNumber, adultPrice, childNumber, childPrice, id)
+    });
+}
+
+function addItem(name, adultNumber, adultPrice, childNumber, childPrice, id) {
+    const item = {
+        name: name, adultNumber: adultNumber, adultPrice: adultPrice, childNumber: childNumber, childPrice: childPrice, id: id
+    };
+    if(!isInCart(item.name)) {
+        cart.push(item);
+        console.log(cart);
+        createBasketItems();
+    }   
+}
+
+function createBasketItems() {
+    const ulEl = findUl();
+    const liPrototypeBasket = findPrototype();
+    clearElement(ulEl);
+    ulEl.appendChild(liPrototypeBasket);
+        cart.forEach(function(item) {
+        let newBasketItem = liPrototypeBasket.cloneNode(true);
+        ulEl.appendChild(newBasketItem);
+        newBasketItem.style.display = "block";
+        const newTitle = newBasketItem.querySelector(".summary__name");
+        const pricesSummary = newBasketItem.querySelector(".summary__prices");
+        newTitle.textContent = item.name;
+        const validedChildNumber = validateExcursionFields(item.childNumber);
+        const validedAdultdNumber = validateExcursionFields(item.adultNumber);
+        pricesSummary.textContent = "Dorośli:" + " " + `${validedAdultdNumber}` + "x" + " " + `${item.adultPrice}` + "PLN" + "," + " " + "Dzieci:" + " " + `${validedChildNumber}` + "x" + `${item.childPrice}` + "PLN";
+        newBasketItem.querySelector(".summary__total-price").textContent = getItemTotal(item.childNumber,item.childPrice, item.adultNumber,item.adultPrice) + " " + "PLN";
+        newBasketItem.classList.add('summary__item--new');
+    })
+    document.querySelector(".order__total-price-value").textContent = getfinalTotal() + " " + "PLN";
+}
+
+function validateExcursionFields (num) {
+    if (num === "") {
+        num = 0;
+        }
+    const convertednum = convertNumber(num);
+    if(!isCorrectNumber(convertednum)) {
+        alert ("Mozesz podac tylko liczbe. Jedna z wartosci nie jest liczba");
+    }
+    return convertednum;
+}
+
+function convertNumber (num) {
+    const convertedNum = parseFloat(num);
+    return convertedNum;
+}
+
+function isCorrectNumber (num) {
+    if(Number.isNaN(Number(num))) {
+        return false;
+    }
+    return true;
+}
+
+function getItemTotal (num1, num2, num3, num4) {
+    let result = (num1 * num2) + (num3 * num4)
+    return result;
+}
+
+function getfinalTotal() {
+    let total = 0
+    for (let i = 0; i < cart.length; i += 1) {
+      total += getItemTotal(cart[i].adultNumber, cart[i].adultPrice, cart[i].childNumber, cart[i].childPrice);
+    }
+    return total
+}
+
+
 function findUl() {
-    return document.querySelector(".panel__excursions");
+    return document.querySelector(".panel__summary");
 }
 
 function findPrototype() {
-    return document.querySelector(".excursions__item--prototype");
+    return document.querySelector(".summary__item--prototype");
 }
 
 function getIdFromRoot(item) {
     return item.dataset.id;
+}
+
+function clearElement(element) {
+    element.innerHTML = '';
+}
+
+function isInCart(name) {
+    return cart.indexOf(name) >= 0;
 }
